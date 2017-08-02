c60c127e11f8e9a8bc7d0c989dd042a966149208
一方通行 <784856034@qq.com>
2017-08-02 22:40:44 +0800

    add recall

--- main.py
+++ main.py
@@ -4,17 +4,37 @@ import itchat
 import sys
 import robot
 import getweather
+import re
 
 reload(sys)
 sys.setdefaultencoding("utf8")
 
+#聊天模式
 chat = 0
+#撤回的消息
+retract = ""
+#撤回字典
+msg_dict = {}
 
-#群聊消息接收 isGroupChat=True
+# 群聊消息接收 isGroupChat=True
 @itchat.msg_register(itchat.content.TEXT,isGroupChat=True)
 def ret_chatroom(msg):
 	#print(u'From:'+':'+msg['FromUserName'].encode('utf-8'))
 	#print(msg['ActualNickName'].encode('utf-8')+':'+msg['Text'].encode('utf-8'))
+	
+	msg_id = msg['MsgId'].encode('utf8')
+	msg_time = msg['CreateTime']
+	msg_from = msg['ActualNickName'].encode('utf-8')
+	msg_content = msg['Text'].encode('utf8')
+	msg_dict.update(
+		{
+			msg_id:{
+				"msg_from": msg_from, "msg_time": msg_time,"msg_content": msg_content
+			}
+		}
+	)
+	
+	#回复
 	reply = ""
 	global chat
 	defaultReply = 'I received:'+msg['Text'].encode('utf-8')
@@ -35,6 +55,13 @@ def ret_chatroom(msg):
 		reply = '切换至聊天模式'
 		chat = 2
 		return reply
+	elif msg['Text'].encode('utf-8') == "查看撤回":
+		global retract 
+		if retract == "" or  msg_from != "输入不能为空":
+			reply = "查看不到，实现不了 >.<"
+		else :
+			reply = retract
+		return reply
 	else:
 		if chat == 0:
 			return
@@ -47,6 +74,18 @@ def ret_chatroom(msg):
 		print('robot say:'+reply)
 		return reply
 	#return reply or defaultReply
+
+@itchat.msg_register(itchat.content.NOTE,isGroupChat=True)
+def send_msg_helper(msg):
+	print("Note:" + msg['Content'].encode('utf8'))
+	if re.search(r"\<\!\[CDATA\[.*撤回了一条消息\]\]\>", msg['Content'].encode('utf8')) is not None:
+		old_msg_id = re.search("\<msgid\>(.*?)\<\/msgid\>", msg['Content']).group(1)
+		old_msg = msg_dict.get(old_msg_id, {})
+		msg_body = old_msg.get('msg_from') + "->撤回:" + old_msg.get('msg_content')
+		msg_dict.pop(old_msg_id)
+		global retract 
+		retract = msg_body
+
 
 def main():
 	itchat.auto_login(enableCmdQR=True,hotReload=True)
--- test.py
+++ test.py
@@ -1,11 +1,35 @@
 #coding=utf8
 import itchat
+import re
 
+msg_dict = {}
 
-@itchat.msg_register(itchat.content.TEXT,isGroupChat=True)
+@itchat.msg_register(itchat.content.TEXT)#,isGroupChat=True)
 def ret_msg(msg):
+	msg_id = msg['MsgId'].encode('utf8')
+	msg_time = msg['CreateTime']
+	msg_from = (itchat.search_friends(userName=msg['FromUserName']))["NickName"].encode('utf8')
+	msg_content = msg['Text'].encode('utf8')
+	msg_dict.update(
+		{
+			msg_id:{
+				"msg_from": msg_from, "msg_time": msg_time,"msg_content": msg_content
+			}
+		}
+	)
+
 	defaultReply = 'I received: ' + msg['Text']
 	return defaultReply
+@itchat.msg_register(itchat.content.NOTE)
+def send_msg_helper(msg):
+	print(msg['Content'].encode('utf8'))
+	if re.search(r"\<\!\[CDATA\[.*撤回了一条消息\]\]\>", msg['Content'].encode('utf8')) is not None:
+		print("xxx")
+		old_msg_id = re.search("\<msgid\>(.*?)\<\/msgid\>", msg['Content']).group(1)
+		old_msg = msg_dict.get(old_msg_id, {})
+		msg_body = old_msg.get('msg_from') + " 撤回->" + old_msg.get('msg_content')
+		msg_dict.pop(old_msg_id)
+		print(msg_body)
 def main():
 	itchat.auto_login(enableCmdQR=True)
 	itchat.run()
